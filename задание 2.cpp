7caa11cb6c4455e4e37064c4be9cadbbbc9de57d
Келин Кирилл ТСА-141 <160851798+KelinKA@users.noreply.github.com>
2024-06-16 14:53:03 +0300

    переделанное  задание 2.cpp

--- задание 2.cpp
+++ задание 2.cpp
@@ -4,85 +4,81 @@ using namespace std;
 
 /**
  * @brief считывает значение из потокового ввода
- * @return значение
+ * @return возвращает введенное значение
  */
 double getValue();
  /**
-  * @brief находит площадь прямоугольника
-  * @param a - длинна прямоугольника
-  * @param b - ширина прямоугольника
-  * @return площадь фигуры 
+  * @brief вычисляет площадь прямоугольника с заданными сторонами a и b
+  * return возвращает значение площади 
   */
 double rectagleSqare(const double a, const double b);
 /**
-  * @brief находит площадь треугольника
-  * @param a - 1 сторона треугольника
-  * @param b - 2 сторона треугольника
-  * @param c - 3 сторона треугольника
-  * @return площадь фигуры 
+  * @brief вычисляет площадь треугольника по формуле полупериметра и сторон треугольника
+  * return возвращает значение площади треугольника
   */
 double triangleSqare(const double a, const double b, const double c);
-/**
-  * @brief проверяет существует ли треугольник
-  * @param a - 1 сторона треугольника
-  * @param b - 2 сторона треугольника
-  * @param c - 3 сторона треугольника
-  * @return 1 если да, 0 если нет
+ /**
+  * @brief проверяет существование треугольника с заданными сторонами
+  * return возвращает true, если треугольник с такими сторонами существует, иначе возвращает false 
   */
-double checkTriangle(const double a, const double b, const double c);
-/**
- * @brief 
- * @return 0 если выполнено успешно, 1 если нет
- */
+bool checkTriangleExistence(const double a, const double b, const double c);
+
+double getValue();
+double rectagleSqare(const double a, const double b);
+double triangleSqare(const double a, const double b, const double c);
+bool checkTriangleExistence(const double a, const double b, const double c);
+
 int main()
 {
-    double a= getValue();
-    cout << "значение а=" << a <<endl;
-    double b= getValue();
-    cout << "значение b=" << b <<endl;
-    double c = getValue();
-    cout << "значение c=" << c <<endl;
-    double k = checkTriangle(a,b,c);
-    if(k=1)
-    {
-        cout << "площадь прямоугольника со сторонами a и b=" << rectagleSqare(a, b) <<endl;
-        cout << "площадь треугольника со сторонами a и b и c=" << triangleSqare(a, b, c) <<endl;
-    }
-   
-    return 0;
+double a = getValue();
+cout << "значение а=" << a << endl;
+double b = getValue();
+cout << "значение b=" << b << endl;
+double c = getValue();
+cout << "значение c=" << c << endl;
+
+if (checkTriangleExistence(a, b, c))
+{
+    cout << "площадь прямоугольника со сторонами a и b=" << rectagleSqare(a, b) << endl;
+    cout << "площадь треугольника со сторонами a и b и c=" << triangleSqare(a, b, c) << endl;
+}
+else
+{
+    cout << "Треугольник с такими сторонами не существует." << endl;
+}
+
+return 0;
 }
 
 double getValue()
 {
-    cout << "введите значение "<< endl;
-    double value;
-    cin>> value;
-    if ( cin.fail())
-    {
-        cout << "error value" << endl;
-        abort();
-    }
-    return value;
-}
-double checkTriangle(const double a, const double b, const double c)
+cout << "введите значение: ";
+double value;
+cin >> value;
+if (cin.fail() || value <= 0)
 {
-    if ((a+b)<c and (b+c)<a and (a+c)<b)
-    {
-        cout <<"треугольник не существует"<< endl;
-        abort();
-    }
-    else
-    {
-        return 0;
-    }
-    return 1;
+cout << "Ошибка ввода значения." << endl;
+abort();
 }
+return value;
+}
+
 double rectagleSqare(const double a, const double b)
 {
-    return a*b;
+return a * b;
 }
+
 double triangleSqare(const double a, const double b, const double c)
 {
-    const double p = ((a + b +c)/2);
-    return (sqrt(p*(p-a)*(p-b)*(p-c)));
+const double p = ((a + b + c) / 2);
+return (sqrt(p * (p - a) * (p - b) * (p - c)));
+}
+
+bool checkTriangleExistence(const double a, const double b, const double c)
+{
+if (a + b > c && a + c > b && b + c > a)
+{
+return true;
+}
+return false;
 }
