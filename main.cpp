f21d918fd2efb3cc5d59ace136f7251ec7f1302a
Келин Кирилл ТСА-141 <160851798+KelinKA@users.noreply.github.com>
2024-06-18 23:31:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,43 +7,32 @@ using namespace std;
  * @brief Проверяет существование треугольника
  * @return true, если треугольник существует, иначе false
 */
-bool isValidTriangle(double a, double b, double c)
-{
-    return (a + b > c) && (a + c > b) && (b + c > a);
-}
-
+bool isValidTriangle(double a, double b, double c);
 /**
  * @brief Проверяет, является ли треугольник прямоугольным
  * @return true, если треугольник прямоугольный, иначе false
 */
-bool isRightTriangle(double a, double b, double c)
-{
-    if (a <= 0 || b <= 0 || c <= 0)
-    {
-        return false;
-    }
-
-    int maxSide = max(max(a, b), c);
-    if (maxSide == a)
-    {
-        return (pow(a, 2) == pow(b, 2) + pow(c, 2));
-    }
-    else if (maxSide = b)
-    {
-        return (pow(b, 2) == pow(a, 2) + pow(c, 2));
-    }
-    else
-    {
-        return (pow(c, 2) == pow(a, 2) + pow(b, 2));
-    }
-}
+bool isRightTriangle(double a, double b, double c);
+/**
+ * @brief считывает значение из потокового ввода
+ * @return возвращает введенное значение
+ */
+double getValue();
+/**
+  * @brief точка входа в программу 
+  * return возвращает 0 в случае успешного завершения программы
+  */
+  int main();
 
 int main()
 {
-    double a, b, c;
-    cout << "Введите длины сторон треугольника: ";
-    cin >> a >> b >> c;
-
+double a = getValue();
+cout << "значение а=" << a << endl;
+double b = getValue();
+cout << "значение b=" << b << endl;
+double c = getValue();
+cout << "значение c=" << c << endl;
+    
     if (!isValidTriangle(a, b, c))
     {
         cout << "Треугольник не существует или введены некорректные данные" << endl;
@@ -59,4 +48,44 @@ int main()
     }
 
     return 0;
+}
+
+bool isValidTriangle(double a, double b, double c)
+{
+    return (a + b > c) && (a + c > b) && (b + c > a);
+}
+
+double getValue()
+{
+cout << "введите значение: ";
+double value;
+cin >> value;
+if (cin.fail() || value <= 0)
+{
+cout << "Ошибка ввода значения." << endl;
+abort();
+}
+return value;
+}
+
+bool isRightTriangle(double a, double b, double c)
+{
+    if (a <= 0 || b <= 0 || c <= 0)
+    {
+        return false;
+    }
+    
+    int maxSide = max(max(a, b), c);
+    if (maxSide == a)
+    {
+        return (pow(a, 2) == pow(b, 2) + pow(c, 2));
+    }
+    else if (maxSide == b)
+        {
+        return (pow(b, 2) == pow(a, 2) + pow(c, 2));
+    }
+    else
+    {
+        return (pow(c, 2) == pow(a, 2) + pow(b, 2));
+    }
 }
